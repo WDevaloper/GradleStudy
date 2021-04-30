d043ea1db682037e439dd476a89196cf818c5373
wangfayouy <f_y_wang6@163.com>
2021-04-30 17:16:38 +0800

    update

--- app/src/main/java/com/github/gradle/mapping/App.java
+++ app/src/main/java/com/github/gradle/mapping/App.java
@@ -1,10 +1,20 @@
 package com.github.gradle.mapping;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
 
 import com.github.router.runtime.Router;
 
 public class App extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
