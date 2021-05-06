2d249ab366a4afc9f2a385e2850010343a69cb5d
wangfayouy <f_y_wang6@163.com>
2021-05-06 18:01:12 +0800

    update

---
+++ app/src/baidu/java/com/github/gradle/BaiduMainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.gradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BaiduMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baidu_main)
+    }
+}
---
+++ app/src/xiaomi/java/com/github/gradle/XiaoMiMainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.gradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class XiaoMiMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_xiao_mi_main)
+    }
+}
