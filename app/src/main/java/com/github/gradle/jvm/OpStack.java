fc36a0556c6bdad6f21bb64e1e1b0d33da8dd015
wangfayouy <f_y_wang6@163.com>
2021-07-06 17:37:02 +0800

    update

---
+++ app/src/main/java/com/github/gradle/jvm/OpStack.java
@@ -0,0 +1,10 @@
+package com.github.gradle.jvm;
+
+public class OpStack {
+    public int add(int i, int j) {
+        int result = 3;
+        result = i + j;
+        return result + 100;
+    }
+}
+
--- app/src/main/java/com/github/gradle/jvm/VMStack.java
+++ app/src/main/java/com/github/gradle/jvm/VMStack.java
@@ -2,8 +2,20 @@ package com.github.gradle.jvm;
 
 public class VMStack {
     public int add(int i, int j) {
+        long longType = 2;
+        double doubleType = 1;
+        int result = 3;
+        result = i + j;
+        return result + 100;
+    }
+
+
+    public static int staticAdd(int i, int j) {
+        long longType = 6;
+        double doubleType = 8;
         int result = 3;
         result = i + j;
         return result + 100;
     }
 }
+
